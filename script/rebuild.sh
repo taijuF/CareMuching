18f374e971bedcb96843a7ae94858944248d1de3
Kztka <paper.sheet.kami@gmail.com>
2016-06-19 02:22:39 +0900

    add script

---
+++ script/rebuild.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+APP_DIR=$PWD
+BACKUP_DIR=$PWD/backup
+mkdir -p $BACKUP_DIR
+YML_URL=$APP_DIR/containers/compose_development.yml
+
+if [ -z "`docker ps -a | grep datastore | awk '{ print $12 }'`" ]; then
+  echo 'make datastore'
+  docker build -t datastore containers/datastore
+  docker run -d --name datastore datastore
+  docker cp db datastore:/usr/src/app
+  docker cp public datastore:/usr/src/app
+else
+  ./script/docker-backup.sh datastore backup/gochiso
+fi
+
+# Docker Stop
+IMG=`docker ps -q`
+if [ -n "$IMG" ]; then
+  docker stop $IMG
+  docker rm $IMG
+fi
+# Delete Images
+RMI_IMG=`docker images | awk '/^<none>/ { print $3 }'`
+if [ -n "$RMI_IMG" ]; then
+  docker rmi $(docker images | awk '/^<none>/ { print $3 }')
+fi
+
+# Build & Up
+docker-compose -f $YML_URL build
+docker-compose -f $YML_URL up -d
