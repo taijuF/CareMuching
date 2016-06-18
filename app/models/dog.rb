c1b52521f64f3cf4551c6c07728354807ff2f16d
shota onishi <showta.0804@gmail.com>
2016-06-19 04:04:19 +0900

    geocoder!!!

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,4 @@
 class Dog < ActiveRecord::Base
+	geocoded_by :facility
+	after_validation :geocode, if: :facility_changed?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20160618132052) do
     t.string   "can_senior"
     t.string   "vaccine"
     t.string   "size"
+    t.float    "latitude"
+    t.float    "longitude"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
